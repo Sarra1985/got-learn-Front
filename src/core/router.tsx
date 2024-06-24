388263e04169817ae7a1f023ad0dcd6509db1bc8
Quentin Jacquet <q.jacquet@gmail.com>
2024-06-24 20:23:45 +0200

    chore(lib): add react router to project (#5)

---
+++ src/core/router.tsx
@@ -0,0 +1,11 @@
+import { Link, createBrowserRouter } from "react-router-dom";
+import App from "../App";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    },
+]);
+
+export { router };
