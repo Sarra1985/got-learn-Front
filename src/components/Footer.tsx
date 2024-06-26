dd9c4a6428bd774b4b2457482b5dfd053164b0ce
Quentin Jacquet <q.jacquet@gmail.com>
2024-06-27 00:32:18 +0200

    feat(footer): init footer style (#7)

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,19 @@
+import Box from "@mui/material/Box";
+import { Colors } from "../core/colors";
+
 export default function Footer() {
   return (
     <footer>
-      <p>Footer</p>
+      <Box
+        height='74px'
+        width="100%"
+        sx={{
+          background: `radial-gradient(at left top, ${Colors.BLEU_OUTREMER}, ${Colors.BLEU_NUIT})`,
+          position: 'fixed',
+          bottom: '0',
+        }}
+      >
+      </Box>
     </footer>
   ) 
 }
--- src/core/colors.ts
+++ src/core/colors.ts
@@ -1,4 +1,7 @@
 export enum Colors {
     PRIMARY= "#1876d1",
     SECONDARY="#ffffff",
+
+    BLEU_OUTREMER="#0D2D45",
+    BLEU_NUIT="#071520",
 }
