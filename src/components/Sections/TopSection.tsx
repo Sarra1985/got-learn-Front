98fcdf08186e8edfa1f88d6cecaef1f0839ef09b
Quentin Jacquet <q.jacquet@gmail.com>
2024-06-27 23:28:36 +0200

    fix(display): wrong background width size

--- src/components/Sections/TopSection.tsx
+++ src/components/Sections/TopSection.tsx
@@ -8,7 +8,6 @@ export default function TopSection() {
       bgcolor={Colors.BLEU_NUIT}
       sx={{
         height: 'calc(100vh - 130px)',
-        width: "100vw",
         backgroundImage: 'url("src/assets/backgrounds/Body_MeshGradient N°1.svg")',
         backgroundRepeat: 'no-repeat',
         backgroundSize: 'contain',
