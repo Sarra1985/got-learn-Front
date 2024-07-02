2b93ba6d93bd50ba4d85b4eba1da116e4651eebc
combesal <138445070+combesal@users.noreply.github.com>
2024-07-02 15:24:36 +0200

    A_UI_002_02: ajouter le logo au header (#17)
    * Ajouter le logo au header
    * Ajouter url landing page + scroll en haut au clic

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,43 @@
+import Box from "@mui/material/Box";
+import { Colors } from "../core/colors";
 import Navbar from "./NavBar/Navbar";
 
 export default function Header() {
 
+  const handleClick = () => {
+    if (window.location.href === '/') {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    } else {
+      window.location.href = '/';
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+  };
+
   return (
-    <header>
-      <img 
-        src="/src/assets/react.svg"
-        alt="image ici" 
+    <Box
+      component="header"
+      bgcolor={Colors.BLEU_NUIT}
+      sx={{
+        width: '100 %',
+        height: '130px',
+        position: 'sticky',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        top: 0,
+      }}
+    >
+      <img
+        onClick={handleClick}
+        src="/src/assets/LOGO_TxtBlc_99px_desvg.webp"
+        alt="Logo Guild Open Tech"
         style={{
-          width: '100px'
+          width: '99px',
+          marginLeft: "58px",
+          cursor: 'pointer',
         }}
       />
       <Navbar />
-    </header>
-  ) 
+    </Box>
+  )
 }
